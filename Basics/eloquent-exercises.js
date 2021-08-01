a5fc4d62937d66c379ceaa2bf5121da1596a6555
Happy Singh <happysingh23828@gmail.com>
2021-08-01 20:32:26 +0530

    Program structure excercise solved

--- Basics/eloquent-exercises.js
+++ Basics/eloquent-exercises.js
@@ -1,1 +1,45 @@
-console.log("hello world from happy")
+
+
+// loopTriangle(5);
+// fizzBuzz(20)
+
+
+function loopTriangle(lines) {
+
+	var outputString = "";
+
+	for (var i = 0; i < lines; i++) {
+		for(var j = 0; j <= i; j++) {
+			outputString+="#"
+		}
+		outputString+="\n"
+	}
+
+	console.log(outputString);
+
+}
+
+function fizzBuzz(maxNumber) {
+
+	for (let i = 1; i <= maxNumber; i++) {
+	
+		var outputString = ""
+		if (i % 3 == 0) {
+			outputString+="Fizz"
+		}
+
+		if (i % 5 == 0) {
+			outputString+="Buzz"
+		}
+
+		if(outputString.length == 0) {
+			console.log(i)
+		} else {
+			console.log(outputString)
+		}
+	}
+
+}
+
+
+
